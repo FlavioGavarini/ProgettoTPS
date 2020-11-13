309ef65aff76d0435b28e6ff40dcfedc0bb64989
Raouffo <60599264+Raouffo@users.noreply.github.com>
2020-11-13 12:04:21 +0100

    aggiunta del file java

--- scriptProj.js
+++ scriptProj.js
@@ -1,14 +1,26 @@
+var table = document.getElementById("tab");
+				var cells = table.getElementsByTagName("td");
+				var tot = 0;
+				for (var i = 0; i < (cells.length -1); i++) 
+				{
+					var v = cells[i].textContent;
+					if (! isNaN(v))  
+					{
+						v = parseInt(v); // parseInt converte in numeri ma non ritorno un valore booleano testabile nel IF
+						tot = tot + v;
+					}
+				}
+				document.getElementById("totale").textContent = tot;
 
-        var table = document.getElementById("tab");
-        var cells = table.getElementsByTagName("td");
-        var tot = 0;
-        for (var i = 0; i < (cells.length -1); i++) 
-        {
-            var v = cells[i].textContent;
-            if (! isNaN(v))  // isNaN valuta se può essere convertito in numero ma non lo converte
-            {
-                v = parseInt(v); // parseInt converte in numeri ma non ritorno un valore booleano testabile nel IF
-                tot = tot + v;
-            }
-        }
-        document.getElementById("totale").textContent = tot;
+
+
+				var prevScrollpos = window.pageYOffset;
+                window.onscroll = function() {
+                var currentScrollPos = window.pageYOffset;
+                if (prevScrollpos > currentScrollPos) {
+                    document.getElementById("navbar").style.top = "0";
+                } else {
+                    document.getElementById("navbar").style.top = "-60px";
+                }
+                prevScrollpos = currentScrollPos;
+            	}
